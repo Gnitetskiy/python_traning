36f7b0b3ea3b2f2f7676eff7c3bbea532e5f4d27
Gnitetskiy Alexey <kennyislife@mail.ru>
2025-02-16 13:16:25 +0300

    Добавление фикстуры в функцию и использование этой функции в тесте контактов

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,91 +1,21 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
+from fixture_contact import Fixture_Contact
 from new_contact_data import New_Contact_Data
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Chrome()
-        self.wd.implicitly_wait(30)
-    
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.open_add_new_page(wd)
-        self.add_new_contact(wd, New_Contact_Data(firstname = "Alex", lastname = "Ivanov", address = "Mockow City", email1 = "testmail@mail.ru", email2 = "testmail2@mail.ru", email3 = "testmail3@mail.ru", homephone ="12345", mobilephone = "54321", workphone = "67890", fax = "09876"))
-        self.return_to_home_page(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_home_page(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def add_new_contact(self, wd, New_Contact_Data):
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(New_Contact_Data.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(New_Contact_Data.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(New_Contact_Data.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(New_Contact_Data.email1)
-        wd.find_element_by_name("email2").click()
-        wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(New_Contact_Data.email2)
-        wd.find_element_by_name("email3").click()
-        wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(New_Contact_Data.email3)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(New_Contact_Data.homephone)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(New_Contact_Data.mobilephone)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(New_Contact_Data.workphone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(New_Contact_Data.fax)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-
-    def open_add_new_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost:8080/addressbook/")
-
-    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd()
-        except NoAlertPresentException as e: return False
-        return True
+@pytest.fixture
+def fixture_contact(request):
+    fixture = Fixture_Contact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
     
-    def tearDown(self):
-        self.wd.quit()
+def test_add_contact(fixture_contact):
+    fixture_contact.open_home_page()
+    fixture_contact.login()
+    fixture_contact.open_add_new_page()
+    fixture_contact.add_new_contact(New_Contact_Data(firstname = "Alex", lastname = "Ivanov", address = "Mockow City", email1 = "testmail@mail.ru", email2 = "testmail2@mail.ru", email3 = "testmail3@mail.ru", homephone ="12345", mobilephone = "54321", workphone = "67890", fax = "09876"))
+    fixture_contact.return_to_home_page()
+    fixture_contact.logout()
 
-if __name__ == "__main__":
-    unittest.main()
+
