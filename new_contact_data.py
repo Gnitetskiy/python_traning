71337acaed7e36fcc7b1d6c89ec23d3d51df93ed
Gnitetskiy Alexey <kennyislife@mail.ru>
2025-02-16 11:17:05 +0300

    Новый класс для хранения свойст контакта

---
+++ new_contact_data.py
@@ -0,0 +1,12 @@
+class New_Contact_Data:
+    def __init__(self, firstname, lastname, address, email1, email2, email3, homephone, mobilephone, workphone, fax):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
+        self.email1 = email1
+        self.email2 = email2
+        self.email3 = email3
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.fax = fax
