0c612ea298448597172425eb462c4b22959619ea
Gnitetskiy Alexey <kennyislife@mail.ru>
2025-02-23 02:43:20 +0300

    добавление теста на изменение группы + доработка класса помошников группы

---
+++ test/test_upd_group.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+def test_upd_first_group(app):
+    app.session.login( username="admin", password="secret")
+    app.group.update_first_group(Group(name="upd test 3", header="upd test header 3", footer="upd test footer 3"))
+    app.session.logout()
