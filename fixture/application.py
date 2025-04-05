88af3d975dcda05dc212017452056b2712d039d1
Gnitetskiy Alexey <kennyislife@mail.ru>
2025-04-05 21:00:58 +0300

    Реализованы опции командной строки, позволяющие указывать тип браузера и адрес тестируемого приложения при запуске тестов

--- fixture/application.py
+++ fixture/application.py
@@ -5,15 +5,23 @@ from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Chrome()
+    def __init__(self, browser, base_url ):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognozed browser %s" % browser)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url= base_url
 
     def open_home_page(self):
         wd = self.wd
-        wd.get("http://localhost:8080/addressbook/")
+        wd.get(self.base_url)
 
 
     def is_valid(self):
