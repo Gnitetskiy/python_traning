1063c02fdc0a2f90aae7cecef15b9be26ffa5848
Gnitetskiy Alexey <kennyislife@mail.ru>
2025-04-06 17:56:10 +0300

    Реализована загрузка тестовых данных из файлов в формате JSON

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,27 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from model.new_contact_data import ContactData
-from test_add_group import random_string
-import string
-import pytest
-import random
-
-def random_phone(maxlen):
-    return [random.choice(string.digits) for i in range(random.randrange(maxlen))]
-
-def random_email(maxlen):
-    symbols = string.ascii_letters+ string.digits +string.punctuation+ " "*10
-    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])+"@mail.ru"
-
-testdata_contact = [ContactData(firstname="", lastname="", address="", email1="",
-                email2="", email3="", homephone="", mobilephone="",
-                workphone="", fax="")]+[
-    ContactData(firstname=random_string("firstname", 5), lastname=random_string("lastname", 10), address=random_string("address", 15),email1=random_email (20),email2=random_email( 25),email3=random_email( 35),homephone=random_phone( 15),mobilephone=random_phone( 15),workphone=random_phone(15),fax=random_phone(15) )
-    for i in (range(5))]
 
 
-@pytest.mark.parametrize("contact", testdata_contact, ids=[repr(x) for x in testdata_contact])
-def test_add_contact(app,contact):
+def test_add_contact(app,json_contacts):
+    contact=json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.open_add_new_page()
     app.contact.fill_contact_data(contact)
