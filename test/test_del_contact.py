847a091aa99ad9d5109af25292d7b732d5c969a8
Gnitetskiy Alexey <agnitetskiy@nobilis.team>
2025-06-01 21:28:26 +0300

    перевод теста удаление контакта на сравнение из БД

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,20 +1,19 @@
 from model.new_contact_data import ContactData
 from random import randrange
 
-def test_delete_some_contact(app):
-    if app.contact.count_contact() ==0:
+def test_delete_some_contact(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contact.open_add_new_page()
         app.contact.fill_contact_data(ContactData(firstname="Alex", lastname="Ivanov", address="Mockow City", email1="testmail@mail.ru",
                         email2="testmail2@mail.ru", email3="testmail3@mail.ru", homephone="12345", mobilephone="54321",
                         workphone="67890", fax="09876"))
         app.contact.enter_add_new_contact()
-    app.contact.open_contact_page()
-    old_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
     index = randrange(len(old_contacts))
     app.contact.delete_contact_by_index(index)
-    new_contacts = app.contact.get_contact_list()
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
-    old_contacts[0:1] = []
+    old_contacts[index:index+1] = []
     assert old_contacts == new_contacts
     assert sorted(old_contacts, key=ContactData.id_or_max) == sorted(new_contacts, key=ContactData.id_or_max)
 
